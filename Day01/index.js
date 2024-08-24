5db70b46c599617a14741d00c3c3be7a79466cb8
jalilseif <jalilseif@yahoo.com>
2024-08-24 13:12:58 +0330

    add solve day 01

---
+++ Day01/index.js
@@ -0,0 +1,13 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    const msg = "Hello World";
+    return function(...args) {
+        return msg;
+    }
+};
+
+  const f = createHelloWorld();
+  f(); // "Hello World"
+
